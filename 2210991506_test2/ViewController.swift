c57e25aa4f9d2f21df38fa3b48d1ac2691783c14
2004Goyal <goyaldhruv7861@gmail.com>
2024-11-23 09:24:24 +0530

    Initial Commit

---
+++ 2210991506_test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2210991506_test2
+//
+//  Created by Dhruv Goyal on 23/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2210991506_test2Tests/_210991506_test2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _210991506_test2Tests.swift
+//  2210991506_test2Tests
+//
+//  Created by Dhruv Goyal on 23/11/24.
+//
+
+import Testing
+@testable import _210991506_test2
+
+struct _210991506_test2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
